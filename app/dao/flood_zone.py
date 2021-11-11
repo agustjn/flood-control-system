111c2864fe1eef98f5642a4f42899ba3b483bfd9
roman18000 <roman18000@gmail.com>
2021-11-10 21:03:43 -0300

    api de zonas inundables

--- app/dao/flood_zone.py
+++ app/dao/flood_zone.py
@@ -1,6 +1,7 @@
 from flask import request
 from app.models.flood_zone import FloodZone
 from app.db import db
+from app.scheme.flood_zone import FloodZoneScheme
 
 class FloodZoneDao():
     
@@ -16,3 +17,11 @@ class FloodZoneDao():
             else:
                 zones =  FloodZone.query.filter(FloodZone.name.like(key_filtered)).filter_by(active = False).paginate(page=page, per_page=items_per_page)
         return zones
+
+    @staticmethod
+    def recover_flood_zones():
+         return FloodZone.query.all()
+
+    @staticmethod
+    def recover_flood_zones_paginated(page,per_page):
+        return FloodZone.query.paginate(page = page, per_page = per_page)
---
+++ app/scheme/flood_zone.py
@@ -0,0 +1,29 @@
+
+from marshmallow import Schema , fields, post_load, validate, ValidationError
+
+
+class CoordinateScheme(Schema):
+    latitude = fields.Str(required=True)
+    longitude = fields.Str(required=True)
+
+
+class FloodZoneScheme(Schema):
+    id = fields.Int(required=True)
+    name = fields.Str(required=True)
+    colour = fields.Str(required=True)
+    coordinates = fields.Nested(CoordinateScheme, many=True, data_key="coordinates")
+        
+class FloodZonePaginationScheme(Schema):
+    page = fields.Int()
+    total = fields.String() 
+    items = fields.Nested(FloodZoneScheme, many=True, data_key="zonas")
+
+
+
+
+
+
+
+flood_zones_scheme = FloodZoneScheme(many = True)
+flood_zone_scheme = FloodZoneScheme()
+flood_zone_pagination_scheme = FloodZonePaginationScheme()
