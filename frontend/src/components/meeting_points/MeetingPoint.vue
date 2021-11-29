5ebcf9780d31fb0f558ffb605b81b97f8b679cae
agustjn <agustin.leidi@hotmail.com>
2021-11-29 14:01:02 -0300

    Puntos de encuentro con su mapa andando

--- frontend/src/components/meeting_points/MeetingPoint.vue
+++ frontend/src/components/meeting_points/MeetingPoint.vue
@@ -2,7 +2,14 @@
 <h1> 
     Meeting Point View
 </h1>
-<Map/>
+
+<div v-if="isCreated">
+    <Map :points_data="points" />
+</div>
+<div v-else>
+    <h1>Cargando mapa..</h1>
+</div>
+
     
 
 </template>
@@ -16,15 +23,24 @@ export default {
     data() {  
         return { 
             points: [ ],
-            errors:[ ]
+            errors:[ ],
+            mapIsCreated: false
         }
     },
     async created() { 
         const response = await fetch("https://admin-grupo3.proyecto2021.linti.unlp.edu.ar/api/points/all");
         const data = await response.json();
-        console.log(data);
-    }
-
+        this.points = data;
+        this.mapIsCreated = true;        
+        
+        
+        
+    },
+    computed: {  
+         isCreated () {  
+            return this.mapIsCreated;
+        }
+    },
 
 }
 </script>
