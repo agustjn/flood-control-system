a216ab2b6e13f67e0fa53afb7c6a034004f65be6
agustjn <agustin.leidi@hotmail.com>
2021-11-29 10:04:24 -0300

    Cambios breves en master

--- frontend/src/components/meeting_points/MeetingPoint.vue
+++ frontend/src/components/meeting_points/MeetingPoint.vue
@@ -2,12 +2,30 @@
 <h1> 
     Meeting Point View
 </h1>
+<Map/>
+    
+
 </template>
 
 
 <script>
+import Map from '../map/Map'
 export default { 
-    
+    name:'meeting-index',
+    components:{Map},
+    data() {  
+        return { 
+            points: [ ],
+            errors:[ ]
+        }
+    },
+    async created() { 
+        const response = await fetch("https://admin-grupo3.proyecto2021.linti.unlp.edu.ar/api/points/all");
+        const data = await response.json();
+        console.log(data);
+    }
+
+
 }
 </script>
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,7 @@ import router from './router';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'leaflet/dist/leaflet.css'
 
 
 // createApp(App).use(router).mount('#app')
