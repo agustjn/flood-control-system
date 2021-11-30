194e2c67db2dc1892e436e5052c7316be8d4e5e8
agustjn <agustin.leidi@hotmail.com>
2021-11-30 11:38:31 -0300

    Revirtiendo

--- frontend/src/components/meeting_points/MeetingPoint.vue
+++ frontend/src/components/meeting_points/MeetingPoint.vue
@@ -2,12 +2,36 @@
 <h1> 
     Meeting Point View
 </h1>
+<<<<<<< HEAD
+=======
+<Map/>
+    
+
+>>>>>>> parent of 5ebcf97... Puntos de encuentro con su mapa andando
 </template>
 
 
 <script>
 export default { 
+<<<<<<< HEAD
     
+=======
+    name:'meeting-index',
+    components:{Map},
+    data() {  
+        return { 
+            points: [ ],
+            errors:[ ]
+        }
+    },
+    async created() { 
+        const response = await fetch("https://admin-grupo3.proyecto2021.linti.unlp.edu.ar/api/points/all");
+        const data = await response.json();
+        console.log(data);
+    }
+
+
+>>>>>>> parent of 5ebcf97... Puntos de encuentro con su mapa andando
 }
 </script>
 
