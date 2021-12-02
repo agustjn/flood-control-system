26a930c925116d0483012b4bf9c446ae5cb7dbcc
agustjn <agustin.leidi@hotmail.com>
2021-12-02 12:10:49 -0300

    Puntos de encuentro y rutas con la lista de informacion

--- web/src/components/routes_of_evacuation/Map.vue
+++ web/src/components/routes_of_evacuation/Map.vue
@@ -1,33 +1,68 @@
 <template>
   <l-map style="height: 400px" :zoom="zoom" :center="center">
     <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-
-            <div v-for="(point, index) in points_data" :key="index">
-            <l-marker :lat-lng="formatCoordinates(point)" name="Hi!!" v-on:click="eventOnClick">
-                <l-tooltip>
-                    <ul>
-                        <li><strong>{{ point.name }}</strong></li>
-                        <li><strong>{{ point.address }}</strong></li>
-                        <li><strong>{{ point.phone }}</strong></li>
-                        <li><strong>{{ point.email }}</strong></li>
-                    </ul>                    
-                </l-tooltip>
-            </l-marker>
-            <l-polyline v-for="(route, index) in routes_data" :lat-lngs="formatRouteCoordinates(route)" color="red" :key="index"></l-polyline>
-                           <!-- <div v-for="(route, index)  in routes_data" :key="index">
-                 <div v-for="(data, index2) in route" :key="index2">
-                    <l-popup v-if="evaluatePopup(index, route.coordinates)" :content="asd" :key="index" />
-                 </div>
-
-             </div> -->
-            </div>
+    <div v-for="(point, index) in points_data" :key="index">
+      <l-marker :lat-lng="formatCoordinates(point)" v-on:click="eventOnClick">
+        <l-tooltip>
+          <ul>
+            <li>
+              <strong>{{ point.name }}</strong>
+            </li>
+            <li>
+              <strong>{{ point.address }}</strong>
+            </li>
+            <li>
+              <strong>{{ point.phone }}</strong>
+            </li>
+            <li>
+              <strong>{{ point.email }}</strong>
+            </li>
+          </ul>
+        </l-tooltip>
+      </l-marker>
+      <l-polyline
+        v-for="(route, index) in routes_data"
+        :lat-lngs="formatRouteCoordinates(route)"
+        color="red"
+        :key="index"
+      ></l-polyline>
+    </div>
   </l-map>
- 
+  <div class="row mt-5">
+    <div class="col-6">
+      <div class="card text-white bg-info mb-3" v-for="(point, index) in points_data" :key="index">
+        <div class="card-header">{{point.name}}</div>
+        <div class="card-body">
+          <h5 class="card-title">{{point.address}}</h5>
+          <p class="card-text">
+            Nro. de telefono: <strong>{{point.phone}}</strong>
+          </p>
+          <p>Email: <strong>{{point.email}}</strong> </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-6">
+      <div class="card" v-for="(route, index) in routes_data" :key="index">
+        <div class="card text-white bg-primary mb-3">
+          <div class="card-header">{{ route.name }}</div>
+          <div class="card-body">
+            <p class="card-text">{{ route.description }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { LMap, LTileLayer, LMarker, LTooltip, LPolyline } from "@vue-leaflet/vue-leaflet";
-
+import {
+  LMap,
+  LTileLayer,
+  LMarker,
+  LTooltip,
+  LPolyline,
+} from "@vue-leaflet/vue-leaflet";
 
 export default {
   components: {
@@ -35,9 +70,9 @@ export default {
     LTileLayer,
     LPolyline,
     LMarker,
-    LTooltip
+    LTooltip,
   },
-  props: ["points_data", "routes_data", "email", "name", "phone" ],
+  props: ["points_data", "routes_data", "email", "name", "phone"],
   data() {
     return {
       url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
@@ -47,25 +82,45 @@ export default {
       center: [-34.9214, -57.9544],
       markerLatLng: [51.504, -0.159],
       address: "",
-      show:false
+      show: false,
     };
   },
   methods: {
     formatCoordinates(point) {
       return [point.coordinates_latitude, point.coordinates_longitude];
     },
-    formatRouteCoordinates(route){  
+    formatRouteCoordinates(route) {
       const coords = [];
-      for (const coor in route.coordinates) {  
+      for (const coor in route.coordinates) {
         coords.push(Object.values(route.coordinates[coor])); // Object.values() convierte el objeto en array [-34,..., -54,...]
       }
       return coords;
     },
-    evaluatePopup(index, coordinates){ 
-      console.log(index==0 || index==coordinates.length);
+    evaluatePopup(index, coordinates) {
+      console.log(index == 0 || index == coordinates.length);
 
-      return index==0 || index==coordinates.length
-    }
+      return index == 0 || index == coordinates.length;
+    },
   },
 };
 </script>
+
+<style scoped>
+.card-header {
+  font-size: 50px;
+}
+
+.card-title {  
+  font-size: 40px;
+}
+
+.card-text { 
+  font-size:25px
+}
+
+strong {  
+  font-size:25px
+}
+
+
+</style>
